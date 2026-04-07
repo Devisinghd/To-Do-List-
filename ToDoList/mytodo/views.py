7c1856acd384e8fbdf2f21271761b9b3588b993c
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-07 18:27:28 +0530

    created detail view

--- ToDoList/mytodo/views.py
+++ ToDoList/mytodo/views.py
@@ -4,3 +4,7 @@ from .models import Jobs
 def main(request):
     tasks = Jobs.objects.all().order_by('-time')
     return render(request,'myapp/index.html',{'tasks':tasks})
+
+def detail(request,id):
+    task = Jobs.objects.get(id=id)
+    return render(request,'myapp/detail.html',{'task':task})
