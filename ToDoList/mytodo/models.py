3e4e28e5b00094308390f78b1b9039d9211242aa
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-08 17:18:53 +0530

    created at field added

--- ToDoList/mytodo/models.py
+++ ToDoList/mytodo/models.py
@@ -3,11 +3,11 @@ from django.utils.timezone import datetime
 # Create your models here.
 class Jobs(models.Model):
 
+    def __str__(self):
+        return self.task
+
     task = models.CharField(max_length=50)
     description =models.TextField(max_length=100)
     time = models.TimeField()
     is_completed = models.BooleanField()
-
-
-    def __str__(self):
-        return self.task
+    created = models.TimeField(auto_now_add=False)
