72c27bb2b7fdfe72adb914ba535243106a2eecbe
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-12 14:41:46 +0530

    view creted for edit and delete

--- ToDoList/mytodo/views.py
+++ ToDoList/mytodo/views.py
@@ -11,12 +11,27 @@ def detail(request,id):
     return render(request,'myapp/detail.html',{'task':task})
 
 def add_task(request):
+    form = tasksForm(request.POST or None)
     if request.method == 'POST':
-        form = tasksForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('mytodo:main')
     else:
         form = tasksForm()
     return render(request,'myapp/add-task.html',{'form':form})
+
+def edit_task(request,id):
+    task = Jobs.objects.get(id=id)
+    form = tasksForm(request.POST or None,instance=task)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('mytodo:main')
+    return render(request,'myapp/add-task.html',{'form':form})
+
+def delete_task(request,id):
+    task = Jobs.objects.get(id=id)
+    task.delete()
+    return redirect('mytodo:main')
+
 
