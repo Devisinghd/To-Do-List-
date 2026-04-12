2fb140b1fbe8da0dedc239cfb918e6aa01273f60
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-12 14:40:55 +0530

    added urls for edit and delete

--- ToDoList/mytodo/urls.py
+++ ToDoList/mytodo/urls.py
@@ -5,5 +5,7 @@ app_name = 'mytodo'
 urlpatterns = [
     path('',views.main,name='main'),
     path('<int:id>/',views.detail,name='detail'),
-    path('add/',views.add_task,name='add_task')
+    path('add/',views.add_task,name='add_task'),
+    path('edit/<int:id>/',views.edit_task,name='edit_task'),
+    path('delete/<int:id>',views.delete_task,name='delete_task'),
 ]
