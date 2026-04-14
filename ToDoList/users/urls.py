70445da948e7f3d4cda4169072dbb2aaac4c66fe
devisingh dangi <devisinghdangi786@gmail.com>
2026-04-14 17:29:57 +0530

    logout and login url

--- ToDoList/users/urls.py
+++ ToDoList/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
 from . import views
 
+app_name = 'users'
 urlpatterns = [
-    path('register/',views.register,name='register'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view,name='logout'),
 ]
