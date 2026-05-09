7382f7af9bb2f89a2684e2738d8f508c89b3e7aa
devisingh dangi <devisinghdangi786@gmail.com>
2026-05-09 14:01:00 +0530

    profile url created

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('mytodo.urls')),
-    path('user/',include('users.urls')),
+    path('', include(('mytodo.urls', 'mytodo'), namespace='mytodo')),
+    path('user/', include(('users.urls', 'users'), namespace='users')),
 ]
